1d4de1e23b90b3e02924b7c28e3a76181d382897
Sergey Oplavin <sergey.oplavin@agiletestware.com>
2017-09-23 15:21:37 +0300

    BUM-151 adapt to changed in bumblebee-client

--- src/main/java/com/agiletestware/bumblebee/uft/UftRunner.java
+++ src/main/java/com/agiletestware/bumblebee/uft/UftRunner.java
@@ -1,10 +1,5 @@
 package com.agiletestware.bumblebee.uft;
 
-import static com.agiletestware.bumblebee.tracking.AttributeNames.DURATION;
-import static com.agiletestware.bumblebee.tracking.AttributeNames.ERROR;
-import static com.agiletestware.bumblebee.tracking.AttributeNames.RETURN_CODE;
-import static com.agiletestware.bumblebee.tracking.AttributeNames.TESTS_RUN;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
@@ -16,6 +11,7 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.math.NumberUtils;
 
 import com.agiletestware.bumblebee.ReportFolderProvider;
 import com.agiletestware.bumblebee.client.api.BumblebeeApi;
@@ -23,18 +19,16 @@ import com.agiletestware.bumblebee.client.api.BumblebeeApiImpl;
 import com.agiletestware.bumblebee.client.runner.ExecutionEnvironment;
 import com.agiletestware.bumblebee.client.runner.Runner;
 import com.agiletestware.bumblebee.client.runner.TestPathFileProvider;
+import com.agiletestware.bumblebee.client.tracking.RunUftTestEvent;
+import com.agiletestware.bumblebee.client.tracking.Tracker;
 import com.agiletestware.bumblebee.client.uftrunner.UftCommandLineBuilder;
 import com.agiletestware.bumblebee.client.uftrunner.UftRunnerParameters;
 import com.agiletestware.bumblebee.client.utils.BuildLogger;
-import com.agiletestware.bumblebee.tracking.ClientType;
-import com.agiletestware.bumblebee.tracking.EventModel;
-import com.agiletestware.bumblebee.tracking.EventName;
 
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.Proc;
 import hudson.model.TaskListener;
-import jersey.repackaged.com.google.common.collect.ImmutableMap;
 
 /**
  * {@link Runner} implementation which runs UFT Batch Runner via cmd.
@@ -66,6 +60,7 @@ public class UftRunner implements Runner<UftRunnerParameters, Integer>, Serializ
 		final long startTime = System.currentTimeMillis();
 		final BumblebeeApi api = new BumblebeeApiImpl(parameters.getBumbleBeeUrl(), (int) TimeUnit.MINUTES.toSeconds(parameters.getTimeOut()));
 		final File report = new File(context.getWorkingDir(), UUID.randomUUID().toString());
+		final Tracker tracker = new Tracker(api);
 		try {
 			parameters.setReportFileName(report.getAbsolutePath());
 			final UftCommandLineBuilder uftBuilder = new UftCommandLineBuilder(parameters, new ReportFolderProvider(new File(workspace.getRemote())),
@@ -82,38 +77,21 @@ public class UftRunner implements Runner<UftRunnerParameters, Integer>, Serializ
 			}
 			final int code = proc.join();
 			stream.println("Return code: " + code);
-			sendTrackingEvent(api, parameters, System.currentTimeMillis() - startTime, report, code, buildLogger);
+			sendTrackingEvent(tracker, parameters, System.currentTimeMillis() - startTime, report, code, buildLogger);
 			return code;
 		} catch (final Exception ex) {
-			sendTrackingEvent(api, ex.getMessage(), parameters.getClientType(), buildLogger);
+			tracker.track(new RunUftTestEvent(parameters.getClientType(), ex.getMessage()), buildLogger);
 			throw ex;
 		} finally {
 			FileUtils.deleteQuietly(report);
 		}
 	}
 
-	private void sendTrackingEvent(final BumblebeeApi api, final UftRunnerParameters params, final long duration, final File report, final int returnCode,
+	private void sendTrackingEvent(final Tracker tracker, final UftRunnerParameters params, final long duration, final File report, final int returnCode,
 			final BuildLogger logger) {
 		try {
-			final String reportContent = new String(Files.readAllBytes(report.toPath()));
-			final EventModel eventModel = new EventModel(EventName.UFT_TEST_RUN, params.getClientType(), ImmutableMap.<String, Object> builder()
-					.put(TESTS_RUN, reportContent)
-					.put(DURATION, duration)
-					.put(RETURN_CODE, returnCode)
-					.build());
-			api.trackAction(eventModel);
-		} catch (final Exception ex) {
-			logger.error("Could not send tracking event: " + ex.getMessage(), ex);
-		}
-	}
-
-	private void sendTrackingEvent(final BumblebeeApi api, final String errorMessage, final ClientType clientType, final BuildLogger logger) {
-		try {
-
-			final EventModel eventModel = new EventModel(EventName.UFT_TEST_RUN, clientType, ImmutableMap.<String, Object> builder()
-					.put(ERROR, errorMessage)
-					.build());
-			api.trackAction(eventModel);
+			final int numberOfTests = NumberUtils.toInt(new String(Files.readAllBytes(report.toPath())));
+			tracker.track(new RunUftTestEvent(params.getClientType(), numberOfTests, returnCode, duration), logger);
 		} catch (final Exception ex) {
 			logger.error("Could not send tracking event: " + ex.getMessage(), ex);
 		}
