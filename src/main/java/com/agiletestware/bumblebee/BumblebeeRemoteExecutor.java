f66103d2265d7b965b7100baffec853798c8fb62
Sergey Oplavin <sergey.oplavin@agiletestware.com>
2018-02-06 11:25:46 +0300

    BUM-91 Log bulk update parameters

--- src/main/java/com/agiletestware/bumblebee/BumblebeeRemoteExecutor.java
+++ src/main/java/com/agiletestware/bumblebee/BumblebeeRemoteExecutor.java
@@ -12,6 +12,7 @@ import org.jenkinsci.remoting.RoleChecker;
 import com.agiletestware.bumblebee.client.api.BulkUpdateParameters;
 import com.agiletestware.bumblebee.client.api.BumblebeeApi;
 import com.agiletestware.bumblebee.client.api.BumblebeeApiImpl;
+import com.agiletestware.bumblebee.util.ThreadLocalMessageFormat;
 
 import hudson.FilePath;
 import hudson.model.TaskListener;
@@ -24,6 +25,7 @@ Callable<Void, Exception>, Serializable {
 	 *
 	 */
 	private static final long serialVersionUID = 3670838509646174454L;
+	private static final ThreadLocalMessageFormat LOG_FORMAT = new ThreadLocalMessageFormat("{0}: {1}");
 
 	private final JenkinsBuildLogger log;
 	private final FilePath workspace;
@@ -49,10 +51,10 @@ Callable<Void, Exception>, Serializable {
 		final List<FilePath> filesToBeUploaded = locateBumbleBeeReports(
 				workspace, parameters.getResultPattern());
 		if (filesToBeUploaded.size() == 0) {
-			throw new Exception("Did not find any file matching the pattern "
+			throw new Exception("Bumblebee: Did not find any file matching the pattern "
 					+ parameters.getResultPattern() + ". Please check pattern");
 		}
-
+		logParameters();
 		try (final BumblebeeApi api = new BumblebeeApiImpl(parameters.getBumbleBeeUrl(),
 				parameters.getTimeOut() * 60)) {
 			for (final FilePath filePath : filesToBeUploaded) {
@@ -67,12 +69,12 @@ Callable<Void, Exception>, Serializable {
 
 		if (errorSeen) {
 			throw new Exception(
-					"[Bumblebee] Could not upload results to HP ALM using the following parameters: " + parameters
+					"Bumblebee: Could not upload results to HP ALM using the following parameters: " + parameters
 					+ " , HP URL " + parameters.getAlmUrl()
 					+ ". Please check settings.");
 		}
 
-		log.info("Upload done");
+		log.info("Bumblebee: Upload done");
 	}
 
 	// XXX legacy code, but working
@@ -107,6 +109,23 @@ Callable<Void, Exception>, Serializable {
 
 	@Override
 	public void checkRoles(final RoleChecker arg0) throws SecurityException {
+	}
+
+	private void logParameters() {
+		log.info("Bumblebee: Uploading results to HP ALM");
+		log.info(LOG_FORMAT.format("Bumblebee URL", parameters.getBumbleBeeUrl()));
+		log.info(LOG_FORMAT.format("HP ALM URL", parameters.getAlmUrl()));
+		log.info(LOG_FORMAT.format("HP ALM User", parameters.getAlmUserName()));
+		log.info(LOG_FORMAT.format("HP ALM Password", "*******"));
+		log.info(LOG_FORMAT.format("HP ALM Domain", parameters.getDomain()));
+		log.info(LOG_FORMAT.format("HP ALM Project", parameters.getProject()));
+		log.info(LOG_FORMAT.format("TestPlan Path", parameters.getTestPlanDirectory()));
+		log.info(LOG_FORMAT.format("TestLab Path", parameters.getTestLabDirectory()));
+		log.info(LOG_FORMAT.format("TestSet Name", parameters.getTestSet()));
+		log.info(LOG_FORMAT.format("Report Format", parameters.getFormat()));
+		log.info(LOG_FORMAT.format("Custom Properties", parameters.getCustomProperties()));
+		log.info(LOG_FORMAT.format("Offline update", parameters.isOffline()));
+		log.info(LOG_FORMAT.format("Timeout", String.valueOf(parameters.getTimeOut())));
 	}
 
 }
